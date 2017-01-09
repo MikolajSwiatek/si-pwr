d8c1e0f287ddd56317f0946fefa5fcbfc89c7aba
Mikołaj Świątek <mikolajswiatek@outlook.com>
2017-01-09 11:17:01 +0100

    Add GA

--- SIPWR/TSP/GeneticAlgorithm.cs
+++ SIPWR/TSP/GeneticAlgorithm.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using SIPWR.TSP.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace SIPWR.TSP
 {
-    public class GeneticAlgorithm
+    public class GeneticAlgorithm : IAlgorithm
     {
+        public int populationSize { get; set; }
+        public double mutationProbability { get; set; }
+        public double recombinationProbability { get; set; }
+        public int termination { get; set; }
+        public int mutator { get; set; }
+
+        public Tour Tour { get; set; }
+
+        private Dictionary<string, Dictionary<string, double>> distances;
+
+        private int uselessGenerations;
+        private CrossingType crossingType;
+        private SelectionType selectionType;
+
+        public GeneticAlgorithm(
+            int populationSize,
+            double mutationProbability,
+            double recombinationProbability,
+            int termination,
+            int mutator,
+            CrossingType crossingType,
+            SelectionType selectionType,
+            List<City> cities)
+        {
+            TourManager.Cities = cities;
+            this.distances = Distance.Calculate(TourManager.Cities);
+            this.populationSize = populationSize;
+            this.mutationProbability = 1.0 / (TourManager.Cities.Count * mutator);
+            this.recombinationProbability = recombinationProbability;
+            this.termination = termination;
+            this.uselessGenerations = 0;
+            this.crossingType = crossingType;
+            this.selectionType = selectionType;
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public History GetResult()
+        {
+            return new History(Tour.TourCities, Tour.Distance);
+        }
+
         public void Initialization()
         {
 
